148b819e19e5d52becc26757f50c5a1181163e25
chakr227 <abhinav.chakraborty@mail.utoronto.ca>
2022-12-07 18:59:28 -0500

    Sound and Slow button update

--- TetrisApp.java
+++ TetrisApp.java
@@ -1,10 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 import model.TetrisModel;
 import views.TetrisView;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
-/** 
+/**
  * A Tetris Application, in JavaFX
  * 
  * Based on the Tetris assignment in the Nifty Assignments Database, authored by Nick Parlante
@@ -13,25 +17,36 @@ public class TetrisApp extends Application {
     TetrisModel model;
     TetrisView view;
 
+
     /** 
      * Main method
      * 
      * @param args agument, if any
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
-    }
 
+    }
+    public static void playMusic() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        InputStream music;
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("Tetris.wav").getAbsoluteFile());
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
     /** 
      * Start method.  Control of application flow is dictated by JavaFX framework
      * 
      * @param primaryStage stage upon which to load GUI elements
      */
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
+        playMusic();
         this.model = new TetrisModel(); // create a model
         this.view = new TetrisView(model, primaryStage); //tie the model to the view
         this.model.startGame(); //begin
+
     }
 
 }
